73d698aa8bcdb09a932eaee0d72fe057bce5a96a
Marco Hermo <marco@silverstripe.com>
2020-09-09 11:09:33 +1200

    silverstripe 3 site type

--- scripts/site-types/silverstripe3.sh
+++ scripts/site-types/silverstripe3.sh
@@ -53,7 +53,7 @@ block="server {
     $rewritesTXT
 
     location / {
-        try_files \$uri /framework/main.php?url=\$uri&\$query_string
+        try_files \$uri /framework/main.php?url=\$uri&\$query_string;
         $headersTXT
     }
 
@@ -68,7 +68,7 @@ block="server {
         location ~ /\. {
             deny all;
         }
-        try_files \$uri /index.php?url=\$uri&\$query_string;
+        try_files \$uri /framework/main.php?url=\$uri&\$query_string;
         $headersTXT
     }
 
